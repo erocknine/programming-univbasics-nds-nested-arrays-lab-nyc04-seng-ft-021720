de0418f392d4821a71de15a448499752eabb009f
erocknine <echeungsk@gmail.com>
2020-01-22 17:57:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,7 +25,7 @@ end
 
 def sorted_matrix
   storage_room.sort()
-  
+  storage_room
 end
 # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
